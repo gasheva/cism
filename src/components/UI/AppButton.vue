c71b06a73e7864030d53f4090a378f51858ec98b
DocGashe <gasheva_99@mail.ru>
2022-11-03 14:47:53 +0500

    Add custom button

---
+++ src/components/UI/AppButton.vue
@@ -0,0 +1,34 @@
+<template>
+    <button class="button" @click="click">{{ label }}</button>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'AppButton'
+};
+</script>
+<script lang="ts" setup>
+const props = defineProps({
+    label: {type: String, default: ''},
+});
+
+const emit = defineEmits(['click']);
+
+const click = () => emit('click');
+</script>
+
+<style lang="scss" scoped>
+.button {
+  width: 100%;
+  padding: 20px 32px;
+  border: 1px solid #0D6EFD;
+  border-radius: 8px;
+
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 17px;
+  color: $blue-500;
+  text-transform: capitalize;
+}
+</style>
