bdebdf30914205217dd8fac4144196c7daac94d8
DocGashe <gasheva_99@mail.ru>
2022-11-03 14:42:11 +0500

    Import scss. Add custom input

---
+++ src/components/UI/AppInput.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="input">
+        <label :for="id" v-if="label" class="input_label">{{ label }}</label>
+        <input :id="id"
+               class="input_input"
+               :class="{'input_input__invalid': !isValid}"
+               :placeholder="placeholder"
+               :type="type"
+               :disabled="isDisabled"
+               v-model="valueDisplayed">
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'AppInput'
+};
+</script>
+<script lang="ts" setup>
+
+import {computed} from 'vue';
+import {generateId} from '@/utils/utils';
+
+const props = defineProps({
+    value: {type: String, default: ''},
+    label: {type: String, default: ''},
+    placeholder: {type: String, default: ''},
+    type: {
+        type: String,
+        default: 'text',
+        validator: (val: string) => {
+            const values = new Set(['text', 'password', 'email']);
+            return values.has(val);
+        }
+    },
+    isDisabled: {type: Boolean, default: false},
+    isValid: {type: Boolean, default: true},
+});
+const emit = defineEmits(['update:value']);
+
+const id = generateId();
+
+const valueDisplayed = computed({
+    get: (): string => props.value,
+    set: (val): void => emit('update:value', val),
+});
+
+
+</script>
+
+<style lang="scss" scoped>
+.input {
+  display: flex;
+  flex-direction: column;
+
+  &_label {
+    line-height: 24px;
+    letter-spacing: 0.25px;
+    color: $dark;
+    text-transform: capitalize;
+  }
+
+  &_input {
+    border: 1.5px solid $gray-200;
+    border-radius: 8px;
+    padding: 16px 24px;
+    background: white;
+    line-height: 17px;
+    color: $secondary;
+
+    &__invalid {
+      border-color: $danger;
+    }
+  }
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,12 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     proxy: process.env.VUE_APP_CISM_API
-  }
+  },
+  css:                   {
+    loaderOptions: {
+      sass: {
+        additionalData: '@import "@/assets/scss/_variables.scss";',
+      },
+    },
+  },
 })
