e1add4f2f15c9064f5ecec8e353fe8ba05b4a42c
DocGashe <gasheva_99@mail.ru>
2022-11-03 16:53:06 +0500

    Add home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,34 @@
 <template>
-    <div>home</div>
+    <div class="home-page">
+        <header class="home-page_header">
+            <h1 class="home-page_username logo">{{ appName }}</h1>
+            <span class="home-page_username">{{ userName }}</span>
+        </header>
+        <app-home/>
+    </div>
 </template>
 
-<script>
+<script lang="ts">
 export default {
-    name: "HomeView"
-}
+    name: 'HomeView'
+};
+</script>
+<script setup lang="ts">
+import AppHome from '@/components/AppHome.vue';
+import {useStore} from 'vuex';
+
+const store = useStore();
+
+const appName = process.env.VUE_APP_NAME;
+const userName = store.getters.getUser.email;
+
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.home-page {
+  &_header {
+    display: flex;
+    justify-content: space-between;
+  }
+}
 </style>
