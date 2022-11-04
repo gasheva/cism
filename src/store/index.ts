073227f94fc9814aa415f4a1ba5d6c930585db6f
DocGashe <gasheva_99@mail.ru>
2022-11-04 15:48:02 +0500

    Fix store return no documents on fetching

--- src/store/index.ts
+++ src/store/index.ts
@@ -61,7 +61,7 @@ export default createStore({
         async requestWrapper(context: ActionContext<State, State>, callback: RequestCallback): Promise<any> {
             context.commit('setIsRequestProcess', true);
             try {
-                await callback.callback(callback.params);
+                return await callback.callback(callback.params);
             } catch (error) {
                 return error;
             } finally {
